36c7a5d06e230cd0907402955bc19e15e938f844
fonkengChris <echristianfonkeng@gmail.com>
2025-09-30 23:48:16 +0100

    Remove initial/fallback data from service user hooks to use purely API data

--- src/hooks/useServiceUser.ts
+++ src/hooks/useServiceUser.ts
@@ -1,24 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 import { serviceUserApi } from "../services/api";
 import { ServiceUser } from "../types/models";
-import initialServiceUsers from "../data/serviceUsers";
 import ms from "ms";
 
 const useServiceUser = (id: string) => {
   return useQuery<ServiceUser, Error>({
     queryKey: ["serviceUser", id],
     queryFn: () => serviceUserApi.getById(id).then((res) => res.data),
-    initialData: () => {
-      const user = initialServiceUsers.find((user) => user._id.$oid === id);
-      if (!user) return undefined;
-      return {
-        ...user,
-        _id: user._id.$oid,
-        dateOfBirth: user.dateOfBirth.$date,
-        createdAt: new Date(user.createdAt.$date),
-        group: user.group?.$oid,
-      } as ServiceUser;
-    },
     enabled: !!id,
     staleTime: ms("1h"),
   });
